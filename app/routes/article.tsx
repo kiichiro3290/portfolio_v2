2e4dd977a346c84fa57af5399bc8b41f1f944260
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2025-03-01 07:47:13 +0900

    add biome

--- app/routes/article.tsx
+++ app/routes/article.tsx
@@ -1,3 +1,15 @@
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = () => {
+  const user = {
+    name: "kai",
+    age: "23",
+  };
+  return json({ user });
+};
+
 export default function Article() {
-  return <div></div>;
+  const { user } = useLoaderData<typeof loader>();
+  return <div>{user.name}</div>;
 }
